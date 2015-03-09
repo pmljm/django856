5dd764f185ca0a11f80d087e7b97cbd9959d5aab
Copser <iamrmot@gmail.com>
2015-03-09 06:04:15 +0100

    Adding static files path, and urls, to settings.py and urls.py

--- source/project_template/settings.py
+++ source/project_template/settings.py
@@ -81,3 +81,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Setting location path to tempaltes
+TEMPLATE_DIRS = [os.path.join(os.path.dirname(BASE_DIR), "static", "templates"), ]
+
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+    STATICFILES_DIRS = [os.path.join(os.path.dirname(BASE_DIR), "static", "static"), ]
