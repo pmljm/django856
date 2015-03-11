94263475a283aa847523f0ba4b602915088778d1
Copser <iamrmot@gmail.com>
2015-03-11 06:27:07 +0100

    Changing db.sqlite3 to mongodb

--- source/project_template/settings.py
+++ source/project_template/settings.py
@@ -58,8 +58,8 @@ WSGI_APPLICATION = 'project_template.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'daryl_database',
     }
 }
 
