7f3e8a8415a6622762376735db54fbbd0bae3f0f
Copser <iamrmot@gmail.com>
2015-03-18 07:15:22 +0100

    Making changes to views.py, adding form to contact and base.html, using crispyforms.

--- source/contacts/views.py
+++ source/contacts/views.py
@@ -9,15 +9,22 @@ def contact(request):
     a email validation to this view, so when future user sign/login or sent a massage,
     you will be receving an email from user to your email.
     Note: You need to define your email credential in django settings.py '''
+    title = 'Contact'
     form = contactForm(request.POST or None)
+    confirm_message = None
+
     if form.is_valid():
         comment = form.cleaned_data['comment']
         name = form.cleaned_data['name']
-        subject = 'Message from yoursite.com'
-        message = '%s %s' %(comment, name)
+        subject = 'Message from MYSITE.com'
+        message = '%s %s' % (comment, name)
         emailFrom = form.cleaned_data['email']
-        emailTo = settings.EMAIL_HOST_USER 
-        send_mail( subject, massage, emailFrom, emailTo, fail_silently=True)
-    context = {}
+        emailTo = [settings.EMAIL_HOST_USER]
+        send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
+        title = "Tanks"
+        confirm_message = "Tanks for the message!"
+        form = None
+
+    context = {'title': title, 'form': form, 'confirm_message': confirm_message}
     template = 'contact.html'
     return render(request, template, context)
